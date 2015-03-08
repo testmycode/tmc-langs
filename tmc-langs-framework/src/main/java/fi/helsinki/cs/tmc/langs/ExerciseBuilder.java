210a7efc44f56f238c94d8087dd547360f82bfdd
Ville Heikkinen <vheikkinen@topcon.com>
2015-03-08 16:52:54 +0200

    Fixed issues of pull request #3

--- tmc-langs-framework/src/main/java/fi/helsinki/cs/tmc/langs/ExerciseBuilder.java
+++ tmc-langs-framework/src/main/java/fi/helsinki/cs/tmc/langs/ExerciseBuilder.java
@@ -2,10 +2,10 @@ package fi.helsinki.cs.tmc.langs;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.io.FileUtils;
 
 public class ExerciseBuilder {
 
@@ -15,26 +15,10 @@ public class ExerciseBuilder {
     private String sourceFolderName = "src";
 
     /**
-     * Constuctor
-     *
-     * <p>
-     * TODO: Needs to be expanded with LanguagePlugin, which determines the
-     * comment style used with the language
-     *
-     */
-    public ExerciseBuilder() {
-
-    }
-
-    /**
      * Prepares a stub exercise from the original.
      *
      * <p>
-     * The stub is a copy of the original where the model solution and special
-     * comments have been stripped and stubs like ('return 0') have been added.
-     *
-     * @param path A path to a directory where the original exericse has been
-     * copied. This method should modify the contents of this directory.
+     * Implements LanguagePlugin.prepareStub
      */
     public void prepareStub(Path path) {
         File projectRoot = path.resolve(sourceFolderName).toFile();
@@ -47,25 +31,25 @@ public class ExerciseBuilder {
 
     private void prepareStubFile(File file) {
         try {
-            List<String> lines = FileUtils.readLines(file);
-            List<String> parsedLines = new ArrayList<>();
+            List<String> lines = Files.readAllLines(file.toPath());
+            List<String> filteredLines = new ArrayList<>();
             boolean skipLine = false;
             for (String line : lines) {
                 if (line.contains(beginSolution)) {
                     skipLine = true;
                 } else if (skipLine && line.contains(endSolution)) {
                     skipLine = false;
-                } else if (!skipLine && !line.contains(stubMarker)) {
-                    parsedLines.add(line);
                 } else if (line.contains(stubMarker)) {
                     String start = line.substring(0, line.indexOf(stubMarker) - 1);
                     String end = line.substring(line.indexOf(stubMarker) + stubMarker.length());
-                    parsedLines.add(start + end);
+                    filteredLines.add(start + end);
+                } else if (!skipLine && !line.contains(stubMarker)) {
+                    filteredLines.add(line);
                 }
             }
-            FileUtils.writeLines(file, parsedLines);
+            Files.write(file.toPath(), filteredLines);
         } catch (IOException ex) {
-            System.out.println("Unexpected IOException, preparation of file {" + file.getAbsolutePath() + "} interrupted");
+            throw new RuntimeException("Unexpected IOException, preparation of file {" + file.getAbsolutePath() + "} interrupted", ex);
         }
     }
 
@@ -89,10 +73,7 @@ public class ExerciseBuilder {
      * Prepares a presentable solution from the original.
      *
      * <p>
-     * The solution usually has stubs and special comments stripped.
-     *
-     * @param path A path to a directory where the original exericse has been
-     * copied. This method should modify the contents of this directory.
+     * Implements LanguagePlugin.preparaSolution
      */
     public void prepareSolution(Path path) {
         File projectRoot = path.resolve(sourceFolderName).toFile();
@@ -102,20 +83,20 @@ public class ExerciseBuilder {
             prepareSolutionFile(projectFile);
         }
     }
-    
+
     private void prepareSolutionFile(File file) {
         try {
-            List<String> lines = FileUtils.readLines(file);
-            List<String> parsedLines = new ArrayList<>();
+            List<String> lines = Files.readAllLines(file.toPath());
+            List<String> filteredLines = new ArrayList<>();
             for (String line : lines) {
                 if (line.contains(beginSolution) || line.contains(endSolution) || line.contains(stubMarker)) {
                     continue;
-                } 
-                parsedLines.add(line);
+                }
+                filteredLines.add(line);
             }
-            FileUtils.writeLines(file, parsedLines);
+            Files.write(file.toPath(), filteredLines);
         } catch (IOException ex) {
-            System.out.println("Unexpected IOException, preparation of file {" + file.getAbsolutePath() + "} interrupted");
+            throw new RuntimeException("Unexpected IOException, preparation of file {" + file.getAbsolutePath() + "} interrupted", ex);
         }
     }
 

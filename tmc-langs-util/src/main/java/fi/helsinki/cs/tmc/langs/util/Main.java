6235f278021faf40ee73f4829cab2b5b7f4a2374
Viding Jasu H <jviding@dat-9.cs.helsinki.fi>
2015-03-10 17:32:42 +0200

    --help for commandline

--- tmc-langs-util/src/main/java/fi/helsinki/cs/tmc/langs/util/Main.java
+++ tmc-langs-util/src/main/java/fi/helsinki/cs/tmc/langs/util/Main.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
+        if (args.length == 0 || args[0].equals("--help")) {
             printHelp();
             System.exit(0);
         }

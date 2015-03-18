4a948bb70cad464030bd2212e8a2a36fce779e8d
jviding <jasu.viding@helsinki.fi>
2015-03-18 22:28:48 +0200

    more tests for Main

--- tmc-langs-util/src/test/java/fi/helsinki/cs/tmc/langs/util/MainTest.java
+++ tmc-langs-util/src/test/java/fi/helsinki/cs/tmc/langs/util/MainTest.java
@@ -11,6 +11,7 @@ import java.io.File;
 import static org.junit.Assert.assertTrue;
 
 public class MainTest {
+
     @Rule
     public MockStdio mio = new MockStdio();
 
@@ -26,6 +27,27 @@ public class MainTest {
         exit.expectSystemExitWithStatus(0);
         exitStringContainsAssertion(HELP_TEXT);
         Main.main(args);
+    }
+    
+    @Test
+    public void testWithNoArgs() {
+        String[] args = {};
+        expectedMessage = HELP_TEXT;
+        mainTest(0, args);
+    }
+    
+    @Test
+    public void testHelp() {
+        String[] args = {"--help"};
+        expectedMessage = HELP_TEXT;
+        mainTest(0, args);
+    }
+    
+    @Test
+    public void testHelpWithInvalidArgumentCountOneArg() {
+        String[] args = {"--help","dummy_string"};
+        expectedMessage = "ERROR: wrong argument count for --help expected 0 got 1\n" + HELP_TEXT;
+        mainTest(0, args);
     }
 
     @Test
@@ -46,9 +68,16 @@ public class MainTest {
     }
 
     @Test
-    public void testWithInvalidArgumentCount() {
+    public void testScanExerciseWithInvalidArgumentCountZeroArgs() {
         String[] args = {"--scanexercise"};
         expectedMessage = "ERROR: wrong argument count for --scanexercise expected 2 got 0\n" + HELP_TEXT;
+        mainTest(0, args);
+    }
+
+    @Test
+    public void testScanExerciseWithInvalidArgumentCountOneArg() {
+        String[] args = {"--scanexercise", "dummy string"};
+        expectedMessage = "ERROR: wrong argument count for --scanexercise expected 2 got 1\n" + HELP_TEXT;
         mainTest(0, args);
     }
 
@@ -69,13 +98,24 @@ public class MainTest {
         expectedMessage = "Codestyle report can be found at " + outputPath;
         mainTest(0, args);
     }
+    
+//    @Test
+//    public void testPrepareStub() {
+//        
+//    }
+    
+//    @Test
+//    public void testPrepareSolution() {
+//        
+//    }
 
     /**
-     * Call main method with given args, and assert that main exits with given exit status.
-     * Additionally do assertion that main prints out {@link #expectedMessage}.
+     * Call main method with given args, and assert that main exits with given
+     * exit status. Additionally do assertion that main prints out
+     * {@link #expectedMessage}.
      *
-     * @param exitStatus           expected exit status for main.
-     * @param args                 for calling main.
+     * @param exitStatus expected exit status for main.
+     * @param args for calling main.
      * @param optionalErrorMessage for system out assertion.
      */
     private void mainTest(int exitStatus, String[] args, String... optionalErrorMessage) {
@@ -98,8 +138,8 @@ public class MainTest {
         exit.checkAssertionAfterwards(new Assertion() {
             @Override
             public void checkAssertion() throws Exception {
-                String defaultErrorMessage = "Expected system output to contain\n" + expected +
-                    "\ninstead got: " + mio.getSysOut().trim();
+                String defaultErrorMessage = "Expected system output to contain\n" + expected
+                        + "\ninstead got: " + mio.getSysOut().trim();
                 if (optionalErrorMessage != null && optionalErrorMessage.length == 1) {
                     assertTrue(optionalErrorMessage[1], mio.getSysOut().trim().contains(expected));
                 } else {

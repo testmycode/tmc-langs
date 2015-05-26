162ef8de6edb44a74df0b47906b1003c46d02759
ljleppan <leo.leppanen@helsinki.fi>
2015-05-26 10:35:40 +0300

    Make utils aware of Maven Plugin

--- tmc-langs-util/src/main/java/fi/helsinki/cs/tmc/langs/util/ProjectType.java
+++ tmc-langs-util/src/main/java/fi/helsinki/cs/tmc/langs/util/ProjectType.java
@@ -2,13 +2,16 @@ package fi.helsinki.cs.tmc.langs.util;
 
 import fi.helsinki.cs.tmc.langs.LanguagePlugin;
 import fi.helsinki.cs.tmc.langs.ant.AntPlugin;
+import fi.helsinki.cs.tmc.langs.maven.MavenPlugin;
 
 /**
  * All the possible project types
  */
 public enum ProjectType {
 
-    JAVA_ANT(new AntPlugin());
+    JAVA_ANT(new AntPlugin()),
+    JAVA_MAVEN(new MavenPlugin());
+
 
     private final LanguagePlugin languagePlugin;
 

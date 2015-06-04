e899e93e799d30d499f1caa95b0ad321a03d2d2b
ljleppan <leo.leppanen@helsinki.fi>
2015-06-04 13:51:43 +0300

    Improved test coverage for AbstractJavaPlugin

--- tmc-langs-java/src/test/java/fi/helsinki/cs/tmc/langs/java/AbstractJavaPluginTest.java
+++ tmc-langs-java/src/test/java/fi/helsinki/cs/tmc/langs/java/AbstractJavaPluginTest.java
@@ -1,9 +1,13 @@
 package fi.helsinki.cs.tmc.langs.java;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import fi.helsinki.cs.tmc.langs.CompileResult;
+import fi.helsinki.cs.tmc.langs.java.exception.TestRunnerException;
+import fi.helsinki.cs.tmc.langs.java.exception.TestScannerException;
 import fi.helsinki.cs.tmc.langs.utils.TestUtils;
 import fi.helsinki.cs.tmc.stylerunner.validation.ValidationError;
 import fi.helsinki.cs.tmc.stylerunner.validation.ValidationResult;
@@ -13,11 +17,44 @@ import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
 public class AbstractJavaPluginTest {
+
+    private class StubLanguagePlugin extends AbstractJavaPlugin {
+
+        public StubLanguagePlugin(String testFolderPath) {
+            super(testFolderPath);
+        }
+
+        @Override
+        protected ClassPath getProjectClassPath(Path path) throws IOException {
+            return null;
+        }
+
+        @Override
+        protected CompileResult build(Path projectRootPath) {
+            return new CompileResult(0, new byte[0], new byte[0]);
+        }
+
+        @Override
+        protected File createRunResultFile(Path path) throws TestRunnerException, TestScannerException {
+            return null;
+        }
+
+        @Override
+        protected boolean isExerciseTypeCorrect(Path path) {
+            return false;
+        }
+
+        @Override
+        public String getLanguageName() {
+            return null;
+        }
+    }
 
     private PluginImplLanguagePlugin pluginImpl;
 
@@ -63,5 +100,49 @@ public class AbstractJavaPluginTest {
         Path project = TestUtils.getPath(getClass(), "dummy_project");
         ValidationResult result = pluginImpl.checkCodeStyle(project);
         assertNull(result);
+    }
+
+    @Test
+    public void exceptionOnGetClassPathReturnsAbsentDuringScanExercise() {
+        Path path = TestUtils.getPath(getClass(), "trivial");
+        AbstractJavaPlugin plugin = new StubLanguagePlugin(path.toString()) {
+            @Override
+            protected boolean isExerciseTypeCorrect(Path path) {
+                return true;
+            }
+
+            @Override
+            protected ClassPath getProjectClassPath(Path path) throws IOException {
+                throw new IOException();
+            }
+        };
+
+        assertFalse(plugin.scanExercise(path, "").isPresent());
+    }
+
+    @Test
+    public void testRunnerExceptionDuringRunTestsReturnsNull() {
+        AbstractJavaPlugin plugin = new StubLanguagePlugin("") {
+            @Override
+            protected File createRunResultFile(Path path)
+                    throws TestRunnerException, TestScannerException {
+                throw new TestRunnerException();
+            }
+        };
+
+        assertNull(plugin.runTests(null));
+    }
+
+    @Test
+    public void testScannerExceptionDuringRunTestsReturnsNull() {
+        AbstractJavaPlugin plugin = new StubLanguagePlugin("") {
+            @Override
+            protected File createRunResultFile(Path path)
+                    throws TestRunnerException, TestScannerException {
+                throw new TestScannerException();
+            }
+        };
+
+        assertNull(plugin.runTests(null));
     }
 }

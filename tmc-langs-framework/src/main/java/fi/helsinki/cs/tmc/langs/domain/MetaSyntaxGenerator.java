a9104a80a2c870e09207d319f15bc2982449d48b
Jarmo Isotalo <jamo@isotalo.fi>
2016-01-24 16:42:59 +0200

    Cache regex creation partially

--- tmc-langs-framework/src/main/java/fi/helsinki/cs/tmc/langs/domain/MetaSyntaxGenerator.java
+++ tmc-langs-framework/src/main/java/fi/helsinki/cs/tmc/langs/domain/MetaSyntaxGenerator.java
@@ -1,11 +1,18 @@
 package fi.helsinki.cs.tmc.langs.domain;
 
+import com.google.common.collect.Maps;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MetaSyntaxGenerator {
 
+    private static final Map<String, List<MetaSyntax>> cache = Maps.newHashMap();
+
     public static List<MetaSyntax> listSyntaxes(String fileType) {
+        if (cache.containsKey(fileType)) {
+            return cache.get(fileType);
+        }
         List<MetaSyntax> list = new ArrayList<>();
         if (fileType.matches("java|c|cpp|h|hpp|js|css")) {
             list.add(new MetaSyntax("\\/\\/", "")); //
@@ -17,6 +24,7 @@ public class MetaSyntaxGenerator {
         if (fileType.matches("properties")) {
             list.add(new MetaSyntax("#", ""));
         }
+        cache.put(fileType, list);
         return list;
     }
 }

1b9946b7572f62180544e888b96191f5b13604f4
Jarmo Isotalo <jamo@isotalo.fi>
2016-01-26 15:50:59 +0200

    Add NoTests plugin support

---
+++ tmc-langs-notests/src/main/java/fi/helsinki/cs/tmc/langs/NoTestsPlugin.java
@@ -0,0 +1,115 @@
+
+package fi.helsinki.cs.tmc.langs;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import fi.helsinki.cs.tmc.langs.abstraction.Strategy;
+import fi.helsinki.cs.tmc.langs.abstraction.ValidationError;
+import fi.helsinki.cs.tmc.langs.abstraction.ValidationResult;
+import fi.helsinki.cs.tmc.langs.domain.ExerciseBuilder;
+import fi.helsinki.cs.tmc.langs.domain.ExerciseDesc;
+import fi.helsinki.cs.tmc.langs.domain.RunResult;
+import fi.helsinki.cs.tmc.langs.domain.TestDesc;
+import fi.helsinki.cs.tmc.langs.domain.TestResult;
+import fi.helsinki.cs.tmc.langs.io.EverythingIsStudentFileStudentFilePolicy;
+import fi.helsinki.cs.tmc.langs.io.StudentFilePolicy;
+import fi.helsinki.cs.tmc.langs.io.sandbox.StudentFileAwareSubmissionProcessor;
+import fi.helsinki.cs.tmc.langs.io.sandbox.SubmissionProcessor;
+import fi.helsinki.cs.tmc.langs.io.zip.StudentFileAwareUnzipper;
+import fi.helsinki.cs.tmc.langs.io.zip.StudentFileAwareZipper;
+import fi.helsinki.cs.tmc.langs.io.zip.Unzipper;
+import fi.helsinki.cs.tmc.langs.io.zip.Zipper;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author jamo
+ */
+public class NoTestsPlugin extends AbstractLanguagePlugin {
+
+    private static final Logger log = LoggerFactory.getLogger(NoTestsPlugin.class);
+
+    public NoTestsPlugin() {
+        super(
+                new ExerciseBuilder(),
+                new StudentFileAwareSubmissionProcessor(),
+                new StudentFileAwareZipper(),
+                new StudentFileAwareUnzipper());
+    }
+
+    public NoTestsPlugin(
+            ExerciseBuilder exerciseBuilder,
+            SubmissionProcessor submissionProcessor,
+            Zipper zipper,
+            Unzipper unzipper) {
+        super(exerciseBuilder, submissionProcessor, zipper, unzipper);
+    }
+
+    @Override
+    public boolean isExerciseTypeCorrect(Path path) {
+        return getConfiguration(path).isSet("no-tests");
+    }
+
+    @Override
+    protected StudentFilePolicy getStudentFilePolicy(Path projectPath) {
+        return new EverythingIsStudentFileStudentFilePolicy();
+    }
+
+    @Override
+    public String getPluginName() {
+        return "No-Tests";
+    }
+
+    @Override
+    public Optional<ExerciseDesc> scanExercise(Path path, String exerciseName) {
+        TestDesc test = new TestDesc(getExerciseName(path) + "Test", getPoints(path));
+        return Optional.of(
+                new ExerciseDesc(getExerciseName(path), ImmutableList.<TestDesc>of(test)));
+    }
+
+    private ImmutableList<String> getPoints(Path path) {
+        return ImmutableList.copyOf(getConfiguration(path).get("no-tests.points").asList());
+    }
+
+    @Override
+    public RunResult runTests(Path path) {
+        TestResult fakeResult =
+                new TestResult(
+                        "Default test", true, getPoints(path), "", ImmutableList.<String>of());
+        return new RunResult(
+                RunResult.Status.PASSED,
+                ImmutableList.<TestResult>of(fakeResult),
+                ImmutableMap.<String, byte[]>of());
+    }
+
+    @Override
+    public ValidationResult checkCodeStyle(Path path, Locale messageLocale)
+            throws UnsupportedOperationException {
+        return new ValidationResult() {
+            @Override
+            public Strategy getStrategy() {
+                return Strategy.DISABLED;
+            }
+
+            @Override
+            public Map<File, List<ValidationError>> getValidationErrors() {
+                return Maps.newHashMap();
+            }
+        };
+    }
+
+    @Override
+    public void clean(Path path) {}
+
+    private String getExerciseName(Path path) {
+        return getConfiguration(path).get("no-tests.exercise_name").asString();
+    }
+}
---
+++ tmc-langs-notests/src/test/java/fi/helsinki/cs/tmc/langs/NoTestsPluginTest.java
@@ -0,0 +1,38 @@
+package fi.helsinki.cs.tmc.langs;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import fi.helsinki.cs.tmc.langs.domain.ExerciseDesc;
+import fi.helsinki.cs.tmc.langs.domain.RunResult;
+import fi.helsinki.cs.tmc.langs.utils.TestUtils;
+
+import com.google.common.collect.ImmutableList;
+
+import org.junit.Test;
+
+import java.nio.file.Path;
+
+public class NoTestsPluginTest {
+
+    NoTestsPlugin plugin;
+
+    public NoTestsPluginTest() {
+        this.plugin = new NoTestsPlugin();
+    }
+
+    @Test
+    public void readsConfigsProperly() {
+        Path project = TestUtils.getPath(getClass(), "notests");
+        System.out.println(project.toAbsolutePath().toString());
+        assertTrue(plugin.isExerciseTypeCorrect(project));
+        ExerciseDesc desc = plugin.scanExercise(project, "No Tests Exercise").get();
+        assertEquals(1, desc.tests.size());
+        assertEquals(
+                ImmutableList.<String>of("1", "notests").toString(),
+                desc.tests.get(0).points.toString());
+
+        RunResult runResult = plugin.runTests(project);
+        assertEquals(RunResult.Status.PASSED, runResult.status);
+    }
+}

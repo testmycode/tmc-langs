c451e7d689d3b148c0475c45a2e1c87a2807713f
Jarmo Isotalo <jamo@isotalo.fi>
2016-05-17 19:47:47 +0300

    Fix extratest

--- clitest.sh
+++ clitest.sh
@@ -2,9 +2,9 @@
 
 mvn clean  package -Dmaven.test.skip=true
 
-java -jar tmc-langs-cli/target/tmc-langs-cli-1.0-SNAPSHOT.jar scan-exercise  --exercisePath tmc-langs-java/src/test/resources/maven_exercise --outputPath a
+java -jar tmc-langs-cli/target/tmc-langs-cli-0.1.3-SNAPSHOT.jar scan-exercise  --exercisePath tmc-langs-java/src/test/resources/maven_exercise --outputPath a
 cat a
 
-java -jar tmc-langs-cli/target/tmc-langs-cli-1.0-SNAPSHOT.jar prepare-stubs --exercisePath tmc-langs-java/src/test/resources/ant_arith_funcs/ --outputPath arithfuncs-proj
-java -jar tmc-langs-cli/target/tmc-langs-cli-1.0-SNAPSHOT.jar checkstyle --exercisePath tmc-langs-cli/src/test/resources/arith_funcs --locale=en --outputPath tmp
+java -jar tmc-langs-cli/target/tmc-langs-cli-0.1.3-SNAPSHOT.jar prepare-stubs --exercisePath tmc-langs-java/src/test/resources/ant_arith_funcs/ --outputPath arithfuncs-proj
+java -jar tmc-langs-cli/target/tmc-langs-cli-0.1.3-SNAPSHOT.jar checkstyle --exercisePath tmc-langs-cli/src/test/resources/arith_funcs --locale=en --outputPath tmp
 cat tmp

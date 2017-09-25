5b161f96ed58cfe10b9f4f5b09ee367e2b78ae9d
eerojala <eero.ojala@helsinki.fi>
2017-09-25 18:01:11 +0300

    Created preliminary RPlugin and RStudentFilePolicy classes

---
+++ tmc-langs-r/src/main/java/fi/helsinki/cs/tmc/langs/r/RStudentFilePolicy.java
@@ -0,0 +1,25 @@
+package fi.helsinki.cs.tmc.langs.r;
+
+import fi.helsinki.cs.tmc.langs.io.ConfigurableStudentFilePolicy;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RStudentFilePolicy extends ConfigurableStudentFilePolicy{
+    public RStudentFilePolicy(Path configFileParent) {
+        super(configFileParent);
+    }
+
+    /**
+     * Returns {@code True} for all files in the <tt>projectRoot/R/</tt> directory and other
+     * files required for building the project.
+     *
+     * <p>Will NOT return {@code True} for any test files. If test file modification are part
+     * of the exercise, those test files are whitelisted as <tt>ExtraStudentFiles</tt> and the
+     * decision to include them is made by {@link ConfigurableStudentFilePolicy}.
+     */
+    @Override
+    public boolean isStudentSourceFile(Path path, Path projectRootPath) {
+        return path.startsWith(Paths.get("R"));
+    }
+}

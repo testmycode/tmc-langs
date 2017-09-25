181df352f1daa98c94cb029a568471fade1ad554
Tuomo Lehtonen <tuomo.lehtonen@cs.helsinki.fi>
2017-09-25 14:59:48 +0300

    testin travis

--- old_clitest.sh
+++ old_clitest.sh
@@ -2,7 +2,7 @@
 # The intention is not to really validate the results of the command,
 # but to make note should any commands fail and other packaging failures.
 
-mvn clean package -Dmaven.test.skip=true -pl tmc-langs-r
+mvn clean  package -Dmaven.test.skip=true
 
 java -jar tmc-langs-cli/target/tmc-langs-cli-*-SNAPSHOT.jar scan-exercise  --exercisePath tmc-langs-java/src/test/resources/maven_exercise --outputPath a
 cat a

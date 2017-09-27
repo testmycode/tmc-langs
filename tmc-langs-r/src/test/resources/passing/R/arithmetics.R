f6ff4b1cfd7cae304ab5a4165264ef0df12527f4
eerojala <eero.ojala@helsinki.fi>
2017-09-27 16:56:40 +0300

    Created tests for RStudentFilePolicy

---
+++ tmc-langs-r/src/test/resources/passing/R/arithmetics.R
@@ -0,0 +1,16 @@
+
+add <- function(a, b) {
+  return(a+b)
+}
+
+subtract <- function(a, b) {
+  return(a-b)
+}
+
+multiply <- function(a, b) {
+  return(a*b)
+}
+
+divide <- function(a, b) {
+  return(a/b)
+}

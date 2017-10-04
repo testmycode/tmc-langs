9629e5e25cd79142d0ea5761ea9b0aa68892885b
anttihaap <antti.haapaniemi@helsinki.fi>
2017-10-04 19:31:32 +0300

    runTests.sh uses run_tests function
    runTests.sh uses run_tests function insted of old "with default" function.

--- tmc-langs-r/runTests.sh
+++ tmc-langs-r/runTests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #Currently this script needs to be run at project root!
-/usr/bin/Rscript -e "library(tmcRtestrunner);run_tests_with_default(TRUE)"
+/usr/bin/Rscript -e "library(tmcRtestrunner);run_tests()"

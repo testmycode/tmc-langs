e28e5c467a92f22ce4438b67e78cc432d612e87d
Antti Haapaniemi <antti.haapaniemi@helsinki.fi>
2017-10-04 20:58:21 +0300

    Added forgotten .results.json and changed TestMain to prev version.

--- tmc-langs-r/src/main/java/fi/helsinki/cs/tmc/langs/r/TestMain.java
+++ tmc-langs-r/src/main/java/fi/helsinki/cs/tmc/langs/r/TestMain.java
@@ -2,10 +2,7 @@ package fi.helsinki.cs.tmc.langs.r;
 
 import fi.helsinki.cs.tmc.langs.domain.RunResult;
 import fi.helsinki.cs.tmc.langs.domain.TestResult;
-import fi.helsinki.cs.tmc.langs.utils.ProcessResult;
-import fi.helsinki.cs.tmc.langs.utils.ProcessRunner;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -16,16 +13,15 @@ public class TestMain {
      *
      * @param args Nothing.
      */
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         //For now, add the path you want to test here fully,
         //for example: pathToGithubFolder/tmc-r/example_projects/example_project1
-        String exampleProjectLocation = "/home/antti/rtmc/tmc-r-tester/tmcRtestrunner/tests/testthat/resources/simple_all_tests_pass";
-
-
+        /**String exampleProjectLocation = "/path/to/r-project"
+                + "/example_projects/example_project1";
         Path path = Paths.get(exampleProjectLocation);
         RPlugin rplugin = new RPlugin();
         RunResult runRes = rplugin.runTests(path);
-        printTestResult(runRes);
+        printTestResult(runRes);**/
     }
 
     public static void printTestResult(RunResult rr) {

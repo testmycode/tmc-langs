38687f8c24df64ae816c4b232b165665b6718a23
Tuomo Lehtonen <tuomo.lehtonen@cs.helsinki.fi>
2017-10-09 15:01:25 +0300

    added missing R example projects

---
+++ tmc-langs-r/src/test/resources/simple_source_code_error/tests/testthat/testMain.R
@@ -0,0 +1,20 @@
+library('testthat')
+
+source("../../R/main.R")
+
+pointsForAllTests(c("r1"))
+
+test("RetTrue works.", c("r1.1"), {
+  expect_true(RetTrue())
+})
+
+test("RetOne works.", c("r1.2"), {
+  expect_equal(RetOne(), 1)
+})
+
+test("Add works.", c("r1.3", "r1.4"), {
+  expect_equal(Add(1, 1), 2)
+  expect_equal(Add(0, 1), 1)
+  expect_equal(Add(0, 0), 0)
+  expect_equal(Add(5, 5), 10)
+})
